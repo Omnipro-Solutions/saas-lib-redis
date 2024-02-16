7eaa840b5044e643870126253306902007763aaf
EdwarAlba <edwar.alba@omni.pro>
2024-02-16 16:10:48 -0500

    Add initial files and dependencies

---
+++ setup.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+DESCRIPTION = "Python library designed to be a Redis management"
+VERSION = "0.0.0"
+PACKAGE_NAME = "omni-pro-redis"
+AUTHOR = "OMNI.PRO"
+AUTHOR_EMAIL = "development@omni.pro"
+URL = "https://github.com/Omnipro-Solutions/saas-lib-redis"
+INSTALL_REQUIRES = [
+    "hiredis>=2.2.0,<3.0.0",
+    "redis>=4.0.0,<5.0.0",
+    "fakeredis[json]>=2.0.0,<3.0.0",
+]  # with open(HERE / "requirements.txt") as f:
+#     INSTALL_REQUIRES = f.read().splitlines()
+
+# This call to setup() does all the work
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url=URL,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="MIT",
+    packages=find_packages(exclude=("tests",)),
+    package_data={"": ["*.pyi", "data/*.csv"]},
+    include_package_data=True,
+    install_requires=INSTALL_REQUIRES,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+        ]
+    },
+    test_suite="tests",
+    python_requires=">=3.9",
+)
